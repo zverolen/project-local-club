45d211dc6ac882841ccdb310842483ec0b533581
Lena Zvereva <zverolen@gmail.com>
2024-01-24 10:41:16 +0100

    Update the text

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -4,7 +4,7 @@ export default function Description() {
       <h2>What is Night Flight?</h2>
       <p>Night Flight is a community of Sydney vampires who are sick of staying all creepy and lonely.</p>
       <div>
-        <p>We transform into bats and fly under the stars two or three times per month.</p>
+        <p>We transform into bats and fly under the stars two or three times a month.</p>
         <p>Enjoy the freedom and the fellowship.</p>
         <p>Feel the respect. Feel the support.</p>
       </div>
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -8,13 +8,13 @@ export default function Signup({ handleDemo, isClicked }) {
         <div>
           <div>
             <h3 className="visually-hidden">Date, Time and Place</h3>
-            <p>Sunday, October, 2 11:30 PM</p>
-            <p>Harbour National Park, at the Horny Lighthouse</p>
+            <p>Sunday, October 2, 11:30 PM</p>
+            <p>Harbour National Park, near the Horny Lighthouse</p>
           </div>
           <div>
             
             <form label="Sign up form" action="" hidden={ isClicked }>
-            <p className="visually-hidden">Please, enter the information about you to take part in our next flight</p>
+            <p className="visually-hidden">Please, enter the information about yourself to take part in our next flight</p>
               <div>
                 <label htmlFor="singup-name">How we can call you</label>
                 <input id="singup-name" name="name" autoComplete="username" type="text" />
@@ -25,12 +25,12 @@ export default function Signup({ handleDemo, isClicked }) {
               </div>
               
               <button type="button" id="js-signupButton" onClick={ handleDemo }>Count me in</button>
-              <p>We need to know the participants so no one is left behind.</p>
+              <p>We need to know all participants so that no one is left behind.</p>
             </form>
             <div aria-live="polite">
               <div hidden={ !isClicked }>
                 <p>DEVELOPER NOTE: THIS HIDES THE CTA LINK IN THE FOOTER.</p>
-                <p>You are added to the next Night Flight event! We sent you an email with all details.</p>
+                <p>You have been added to the next Night Flight event! We have sent you an email with all the details.</p>
               </div>
             </div>
           </div>
