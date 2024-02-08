a84e501077d25b5db8eec4502606bdbc5f1bdb7d
Lena Zvereva <zverolen@gmail.com>
2024-02-08 13:48:40 +0100

    Add rems to responsive images

--- src/components/images/batPhoto.jsx
+++ src/components/images/batPhoto.jsx
@@ -4,7 +4,7 @@ export default function BatPhoto({batImage}) {
   return (
     <div>
        <img
-        sizes="(max-width: 600px) 23vw, (max-width: 1440px) 13vw, 188px"
+        sizes="(max-width: calc(600 / 16 * 1rem)) 23vw, (max-width: calc(1440 / 16 * 1rem)) 13vw, 11.75rem"
         srcSet={`./images/${batImage.imageToken}_74.jpg 74w,
                 ./images/${batImage.imageToken}_376.jpg 376w`}
         src={`./images/${batImage.imageToken}_74.jpg`}
